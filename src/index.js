40f241257ed711e988479419eda53f33fd185487
Gilmar Vanin <jvanin@thoughtworks.com>
2018-04-13 20:16:22 -0300

    Add cors configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import cors from 'cors'
+
 import * as Products from './services/products'
 
 const app = express()
+
 const routers = express.Router()
 
 routers.get('/products', (req, res) => (
@@ -12,6 +15,6 @@ routers.get('/health', (req, res) => (
   res.status(200).end()
 ))
 
+app.use(cors())
 app.use('/v1', routers)
-
 app.listen(8081)
